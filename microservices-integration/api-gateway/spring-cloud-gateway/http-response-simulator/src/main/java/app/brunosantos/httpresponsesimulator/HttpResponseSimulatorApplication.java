cf52fe58df13f2c3662a65f2d167aa190abedad9
GFT\bont <bruno.santos1>
2025-03-09 14:32:45 -0300

    [POC-11] Create HTTP Response Simulator Service
    - Created a new microservice named 'http-response-simulator'.
    - This service simulates various HTTP responses based on configurable inputs (status codes, headers, response bodies).
    - Implemented endpoints to handle GET and POST requests to simulate predefined responses.
    - Added a 'GET /status' endpoint to check the health/status of the service.
    - The service is designed to be easily configurable for testing purposes and integrates seamlessly with the API Gateway.

---
+++ microservices-integration/api-gateway/spring-cloud-gateway/http-response-simulator/src/main/java/app/brunosantos/httpresponsesimulator/HttpResponseSimulatorApplication.java
@@ -0,0 +1,13 @@
+package app.brunosantos.httpresponsesimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpResponseSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpResponseSimulatorApplication.class, args);
+	}
+
+}
